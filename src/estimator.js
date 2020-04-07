7030558562bbaf7b7148ad7c2c2eb6f3a653bc25
oluwadareseyi <oluwadareseyii@gmail.com>
2020-04-07 17:52:24 +0100

    test challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,31 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const impact = {};
+  const severeImpact = {};
+  impact.currentlyInfected = data.reportedCases * 10;
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+  const power = Number((data.timeToElapse / 3).toFixed(0));
+  impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** power;
+  severeImpact.infectionsByRequestedTime =
+    severeImpact.currentlyInfected * 2 ** power;
+  return {
+    data,
+    impact,
+    severeImpact
+  };
+};
 
 export default covid19ImpactEstimator;
+
+// const repObj = {
+//   region: {
+//     name: 'Africa',
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 5,
+//     avgDailyIncomePopulation: 0.71
+//   },
+//   periodType: 'days',
+//   timeToElapse: 58,
+//   reportedCases: 674,
+//   population: 66622705,
+//   totalHospitalBeds: 1380614
+// };
