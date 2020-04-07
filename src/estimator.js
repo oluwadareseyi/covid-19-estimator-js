03ecd11ca12f395c91c8f4507f9f6c30459850c7
oluwadareseyi <oluwadareseyii@gmail.com>
2020-04-07 18:07:20 +0100

    testing challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -3,7 +3,12 @@ const covid19ImpactEstimator = (data) => {
   const severeImpact = {};
   impact.currentlyInfected = data.reportedCases * 10;
   severeImpact.currentlyInfected = data.reportedCases * 50;
-  const power = Number((data.timeToElapse / 3).toFixed(0));
+
+  const periodType = data.periodType;
+  const timeElapse = data.timeToElapse
+
+  const convertToDays = periodType === "months" ? timeElapse * 30 : (periodType === "weeks" ? timeElapse * 7 : timeElapse )
+  const power = parseInt((convertToDays / 3));
   impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** power;
   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 2 ** power;
   return {
@@ -15,16 +20,16 @@ const covid19ImpactEstimator = (data) => {
 
 export default covid19ImpactEstimator;
 
-// const repObj = {
-//   region: {
-//     name: 'Africa',
-//     avgAge: 19.7,
-//     avgDailyIncomeInUSD: 5,
-//     avgDailyIncomePopulation: 0.71
-//   },
-//   periodType: 'days',
-//   timeToElapse: 58,
-//   reportedCases: 674,
-//   population: 66622705,
-//   totalHospitalBeds: 1380614
-// };
+const repObj = {
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+};
