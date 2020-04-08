3718a47445750a04984421ebe756f74585545255
oluwadareseyi <oluwadareseyii@gmail.com>
2020-04-08 03:13:33 +0100

    backend v5

--- src/estimator.js
+++ src/estimator.js
@@ -60,17 +60,3 @@ const covid19ImpactEstimator = (data) => {
 };
 
 export default covid19ImpactEstimator;
-
-// const repObj = {
-//   region: {
-//     name: 'Africa',
-//     avgAge: 19.7,
-//     avgDailyIncomeInUSD: 5,
-//     avgDailyIncomePopulation: 0.71
-//   },
-//   periodType: 'days',
-//   timeToElapse: 58,
-//   reportedCases: 674,
-//   population: 66622705,
-//   totalHospitalBeds: 1380614
-// };
